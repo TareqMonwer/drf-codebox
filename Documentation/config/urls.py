4cc656083568b608c60d684b6bffe9a021db1987
tareqmonwer <tareqmonwer137@gmail.com>
2020-07-03 02:58:45 +0600

    DRF tutorial-1

--- Documentation/config/urls.py
+++ Documentation/config/urls.py
@@ -28,5 +28,6 @@ router.register(r'groups', GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
     path('', include(router.urls)),
 ]
