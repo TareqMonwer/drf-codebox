7d7bd4c8f90cef3187a33b47bc75903181a535e2
tareqmonwer <tareqmonwer137@gmail.com>
2020-07-03 23:13:46 +0600

    Docs: Requests and Responses tutorial

--- Documentation/config/settings.py
+++ Documentation/config/settings.py
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    # ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
